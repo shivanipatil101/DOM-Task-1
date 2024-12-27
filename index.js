98776ec21f0494003e1b04b423a634c681131fe8
shivanipatil101 <shivanippatil866@gmail.com>
2024-12-27 20:53:25 +0530

    JS file.

---
+++ index.js
@@ -0,0 +1,23 @@
+// Retrieve the target div element
+let targetDiv = document.getElementById("targetDiv");
+
+// Change background color to yellow
+targetDiv.style.backgroundColor = "yellow";
+
+// Add margin of 20 pixels
+targetDiv.style.margin = "20px";
+
+// Add padding of 10 pixels
+targetDiv.style.padding = "10px";
+
+// Change font size to 18 pixels
+targetDiv.style.fontSize = "18px";
+
+// Set font weight to bold
+targetDiv.style.fontWeight = "bold";
+
+// Set height to 200 pixels
+targetDiv.style.height = "200px";
+
+// Set width to 300 pixels
+targetDiv.style.width = "300px";
